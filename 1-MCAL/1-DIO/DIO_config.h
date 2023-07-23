35d55e6dee5da94e9e93eeefe0ae2d65a4b2ea3c
Adham Ibrahim <Adhamibrahim619@gmail.com>
2023-07-23 09:09:23 +0300

    added an MCAL folder

---
+++ 1-MCAL/1-DIO/DIO_config.h
@@ -0,0 +1,40 @@
+/**********************************************************/
+/**********************************************************/
+/***************	Author: Adham Ibrahim	***************/
+/***************	Layer: 	MCAL			***************/
+/***************	SWC: 	DIO				***************/
+/***************	Version:1.00			***************/
+/**********************************************************/
+/**********************************************************/
+
+#ifndef DIO_CONFIG_H_
+#define DIO_CONFIG_H_
+
+#define PIN_INPUT	0
+#define PIN_OUTPUT	1
+
+#define PORT_INPUT	0
+#define PORT_OUTPUT	1
+
+#define PIN_HIGH	1
+#define PIN_LOW		0
+
+#define PORT_HIGH	0xff
+#define PORT_LOW	0
+
+
+#define	DIO_PORTA	0
+#define	DIO_PORTB	1
+#define	DIO_PORTC	2
+#define	DIO_PORTD	3
+
+#define	PIN0		0
+#define	PIN1		1
+#define	PIN2		2
+#define	PIN3		3
+#define	PIN4		4
+#define	PIN5		5
+#define	PIN6		6
+#define	PIN7		7
+
+#endif
