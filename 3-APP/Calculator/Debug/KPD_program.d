1b19dac59fd2eb44cef8c2a0a81bb97173ed09f1
Adham Ibrahim <140270056+Adham-Ibrahim05@users.noreply.github.com>
2023-07-27 21:42:46 +0300

    Add files via upload

---
+++ 3-APP/Calculator/Debug/KPD_program.d
@@ -0,0 +1,17 @@
+KPD_program.d KPD_program.o: .././KPD_program.c .././STD_TYPES.h \
+ .././DIO_interface.h .././DIO_config.h .././KPD_config.h \
+ .././KPD_interface.h .././KPD_register.h .././KPD_private.h
+
+.././STD_TYPES.h:
+
+.././DIO_interface.h:
+
+.././DIO_config.h:
+
+.././KPD_config.h:
+
+.././KPD_interface.h:
+
+.././KPD_register.h:
+
+.././KPD_private.h:
