56f35a4a67abd5e6d4e0a2105fab843dbbf7d15f
Adham Ibrahim <140270056+Adham-Ibrahim05@users.noreply.github.com>
2023-07-27 21:41:00 +0300

    Add files via upload

---
+++ 3-APP/Calculator/Calculator/KPD_config.h
@@ -0,0 +1,33 @@
+/**********************************************************/
+/**********************************************************/
+/***************	Author: Adham Ibrahim	***************/
+/***************	Layer: 	HAL				***************/
+/***************	SWC: 	KPD				***************/
+/***************	Version:1.00			***************/
+/**********************************************************/
+/**********************************************************/
+
+#ifndef KPD_CONFIG_H_
+#define KPD_CONFIG_H_
+
+#define KEY_NOT_PRESSED	0XFF
+
+#define	ROW_PORT		DIO_PORTC
+#define	COLOUMN_PORT	DIO_PORTD
+
+#define	ROW1			PIN5
+#define	ROW2			PIN4
+#define	ROW3			PIN3
+#define	ROW4			PIN2
+
+#define	COLOUMN1		PIN7
+#define	COLOUMN2		PIN6
+#define	COLOUMN3		PIN5
+#define	COLOUMN4		PIN3
+
+#define KPD_ARR_VAL		{{7,8,9,'/'},{4,5,6,'*'},{1,2,3,'-'},{'13',0,'=','+'}}
+						 
+						 
+						 
+
+#endif
